2035830f2d5a92eabfdaa80e4c5e85e8ab7f6765
Felipe Escobar <luis1401cano2005@gmail.com>
2025-08-18 17:22:52 -0500

    Fix: Ajustar configuración de rutas para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import HeroSection from "./components/Hero"
 import Footer from "./components/Footer"

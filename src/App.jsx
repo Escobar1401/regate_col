1be2b16b8f5c5be8d23f50b9d4f5b3f756be016a
Felipe Escobar <luis1401cano2005@gmail.com>
2025-08-18 17:20:28 -0500

    Fix: Ajustar configuración de rutas para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import HeroSection from "./components/Hero"
 import Footer from "./components/Footer"
@@ -8,7 +8,7 @@ import JerseyDetails from "./components/JerseyDetails"
 
 function App() {
     return (
-        <Router>
+        <Router basename="/regate_col">
             <div className="flex flex-col w-screen">
                 <Header />
                 <Routes>
